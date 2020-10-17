d9ee33c864981986f053b78348673294ea2b56ae
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2020-10-17 10:59:54 +0530

    Side Drawer is now functional

--- lib/Screen5.dart
+++ lib/Screen5.dart
@@ -11,14 +11,15 @@ class _Screen5State extends State<Screen5> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: NavDrawer(),
       appBar: AppBar(
         backgroundColor: Colors.pink[600],
         centerTitle: true,
         title: Text("Friends"),
-        leading: Icon(
-          Icons.menu,
-          color: Colors.white,
-        ),
+        // leading: Icon(
+        //   Icons.menu,
+        //   color: Colors.white,
+        // ),
         actions: [
           PopupMenuButton<String>(
             onSelected: handleClick,
@@ -78,5 +79,169 @@ class _Screen5State extends State<Screen5> {
       case 'Settings':
         break;
     }
+  }
+}
+
+// Navdrawert class
+class NavDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Container(
+        color: Colors.pink,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            Container(
+              height: 120,
+              color: Colors.pink,
+              padding: EdgeInsets.all(12),
+              child: Text(
+                "Sushy",
+                style: TextStyle(
+                  letterSpacing: 1.4,
+                  color: Colors.white,
+                  fontSize: 32,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              alignment: Alignment.bottomLeft,
+            ),
+            Ink(
+              color: Colors.pink,
+              child: ListTile(
+                leading:
+                    Icon(Icons.supervised_user_circle, color: Colors.white),
+                title: Text(
+                  'Profile',
+                  style: TextStyle(
+                    letterSpacing: 1.3,
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                onTap: () => {Navigator.pop(context)},
+              ),
+            ),
+            Ink(
+              color: Colors.pink,
+              child: ListTile(
+                leading: Icon(Icons.image_outlined, color: Colors.white),
+                title: Text(
+                  'Gallery',
+                  style: TextStyle(
+                    letterSpacing: 1.3,
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                onTap: () => {Navigator.pop(context)},
+              ),
+            ),
+            Ink(
+              color: Colors.pink,
+              child: ListTile(
+                leading: Icon(Icons.upload_file, color: Colors.white),
+                title: Text(
+                  'Upload',
+                  style: TextStyle(
+                    letterSpacing: 1.3,
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                onTap: () => {Navigator.pop(context)},
+              ),
+            ),
+            Ink(
+              color: Colors.pink,
+              child: ListTile(
+                leading: Icon(Icons.supervised_user_circle_sharp,
+                    color: Colors.white),
+                title: Text(
+                  'Friends',
+                  style: TextStyle(
+                    letterSpacing: 1.3,
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                onTap: () => {Navigator.pop(context)},
+              ),
+            ),
+            Ink(
+              color: Colors.pink,
+              child: ListTile(
+                leading: Icon(Icons.search, color: Colors.white),
+                title: Text(
+                  'Search',
+                  style: TextStyle(
+                    letterSpacing: 1.3,
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                onTap: () => {Navigator.pop(context)},
+              ),
+            ),
+            Ink(
+              color: Colors.pink,
+              child: ListTile(
+                leading: Icon(Icons.notifications_none_outlined,
+                    color: Colors.white),
+                title: Text(
+                  'Notifications',
+                  style: TextStyle(
+                    letterSpacing: 1.3,
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                onTap: () => {Navigator.pop(context)},
+              ),
+            ),
+            Ink(
+              color: Colors.pink,
+              child: ListTile(
+                leading: Icon(Icons.settings_outlined, color: Colors.white),
+                title: Text(
+                  'Settings',
+                  style: TextStyle(
+                    letterSpacing: 1.3,
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                onTap: () => {Navigator.pop(context)},
+              ),
+            ),
+            SizedBox(height: 65),
+            Ink(
+              color: Colors.pink,
+              child: ListTile(
+                leading: Icon(Icons.logout, color: Colors.white),
+                title: Text(
+                  'Log Out',
+                  style: TextStyle(
+                    letterSpacing: 1.3,
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                onTap: () => {Navigator.pop(context)},
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
