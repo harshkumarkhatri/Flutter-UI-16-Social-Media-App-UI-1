e45e8e51c91e2cae5717b05f7ff4a256bdbb912e
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2020-10-18 16:30:49 +0530

    User can now view photos and videos

---
+++ lib/Screen10.dart
@@ -0,0 +1,283 @@
+// This screen is for the video screen.
+
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_ui_16_sushi_social_media_app_ui/Widgets/videoItems.dart';
+import 'package:image_picker/image_picker.dart';
+
+class Screen10 extends StatefulWidget {
+  @override
+  _Screen10State createState() => _Screen10State();
+}
+
+class _Screen10State extends State<Screen10> {
+
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        drawer: NavDrawer(),
+        appBar: AppBar(
+          backgroundColor: Colors.pink[600],
+          centerTitle: true,
+          title: Text("BLOG COMMENTS"),
+          actions: [
+            PopupMenuButton<String>(
+              onSelected: handleClick,
+              itemBuilder: (BuildContext context) {
+                return {'Logout', 'Settings'}.map((String choice) {
+                  return PopupMenuItem<String>(
+                    value: choice,
+                    child: Text(choice),
+                  );
+                }).toList();
+              },
+            ),
+          ],
+          elevation: 10,
+        ),
+        body: DefaultTabController(
+          length: 2,
+          child: Column(
+            children: <Widget>[
+              Container(
+                constraints: BoxConstraints(maxHeight: 150.0),
+                child: Material(
+                  color: Colors.white,
+                  child: TabBar(
+                    indicatorColor: Colors.pink,
+                    unselectedLabelColor: Colors.grey,
+                    labelColor: Colors.grey,
+                    tabs: [
+                      Tab(
+                        child: Text(
+                          "Videos",
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                      Tab(
+                        child: Text(
+                          "Photos",
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TabBarView(
+                  children: [
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(left: 15.0, right: 15, top: 7),
+                      child: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            GestureDetector(
+                                onTap: () {
+                                },
+                                child: videoItem()),
+                            SizedBox(height: 15),
+                            videoItem(),
+                            SizedBox(height: 15),
+                            videoItem(),
+                            SizedBox(height: 15),
+                            videoItem(),
+                            SizedBox(height: 15),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Icon(Icons.directions_transit),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void handleClick(String value) {
+    switch (value) {
+      case 'Logout':
+        break;
+      case 'Settings':
+        break;
+    }
+  }
+}
+
+class NavDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Container(
+        color: Colors.pink,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            Container(
+              height: 120,
+              color: Colors.pink,
+              padding: EdgeInsets.all(12),
+              child: Text(
+                "Sushy",
+                style: TextStyle(
+                  letterSpacing: 1.4,
+                  color: Colors.white,
+                  fontSize: 32,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              alignment: Alignment.bottomLeft,
+            ),
+            Ink(
+              color: Colors.pink,
+              child: ListTile(
+                leading:
+                    Icon(Icons.supervised_user_circle, color: Colors.white),
+                title: Text(
+                  'Profile',
+                  style: TextStyle(
+                    letterSpacing: 1.3,
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                onTap: () => {Navigator.pop(context)},
+              ),
+            ),
+            Ink(
+              color: Colors.pink,
+              child: ListTile(
+                leading: Icon(Icons.image_outlined, color: Colors.white),
+                title: Text(
+                  'Gallery',
+                  style: TextStyle(
+                    letterSpacing: 1.3,
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                onTap: () => {Navigator.pop(context)},
+              ),
+            ),
+            Ink(
+              color: Colors.pink,
+              child: ListTile(
+                leading: Icon(Icons.upload_file, color: Colors.white),
+                title: Text(
+                  'Upload',
+                  style: TextStyle(
+                    letterSpacing: 1.3,
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                onTap: () => {Navigator.pop(context)},
+              ),
+            ),
+            Ink(
+              color: Colors.pink,
+              child: ListTile(
+                leading: Icon(Icons.supervised_user_circle_sharp,
+                    color: Colors.white),
+                title: Text(
+                  'Friends',
+                  style: TextStyle(
+                    letterSpacing: 1.3,
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                onTap: () => {Navigator.pop(context)},
+              ),
+            ),
+            Ink(
+              color: Colors.pink,
+              child: ListTile(
+                leading: Icon(Icons.search, color: Colors.white),
+                title: Text(
+                  'Search',
+                  style: TextStyle(
+                    letterSpacing: 1.3,
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                onTap: () => {Navigator.pop(context)},
+              ),
+            ),
+            Ink(
+              color: Colors.pink,
+              child: ListTile(
+                leading: Icon(Icons.notifications_none_outlined,
+                    color: Colors.white),
+                title: Text(
+                  'Notifications',
+                  style: TextStyle(
+                    letterSpacing: 1.3,
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                onTap: () => {Navigator.pop(context)},
+              ),
+            ),
+            Ink(
+              color: Colors.pink,
+              child: ListTile(
+                leading: Icon(Icons.settings_outlined, color: Colors.white),
+                title: Text(
+                  'Settings',
+                  style: TextStyle(
+                    letterSpacing: 1.3,
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                onTap: () => {Navigator.pop(context)},
+              ),
+            ),
+            SizedBox(height: 65),
+            Ink(
+              color: Colors.pink,
+              child: ListTile(
+                leading: Icon(Icons.logout, color: Colors.white),
+                title: Text(
+                  'Log Out',
+                  style: TextStyle(
+                    letterSpacing: 1.3,
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                onTap: () => {Navigator.pop(context)},
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Widgets/videoItems.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+Widget videoItem() {
+  return Container(
+    height: 200,
+    color: Colors.grey,
+    child: Center(
+      child: Icon(
+        Icons.play_arrow,
+        color: Colors.pink,
+      ),
+    ),
+  );
+}
